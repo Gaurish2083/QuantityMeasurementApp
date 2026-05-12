24763791ed21bf2d7962f9a880f283e77f0f3aaa
Gaurish2083 <Gaurish2083>
2026-05-12 11:38:04 +0530

    Initial/uc10

--- src/main/java/com/quantity/measurement/QuantityMeasurementApp.java
+++ src/main/java/com/quantity/measurement/QuantityMeasurementApp.java
@@ -2,25 +2,17 @@ package com.quantity.measurement;
 
 import com.quantity.measurement.enums.LengthUnit;
 import com.quantity.measurement.model.QuantityLength;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuantityMeasurementApp {
 
+
 	public static void main(String[] args) {
 
-		QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
-		QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCH);
+		QuantityLength q1 = new QuantityLength(2.0, LengthUnit.CENTIMETERS);
+		QuantityLength q2 = new QuantityLength(2.0, LengthUnit.YARDS);
 
-		QuantityLength q3 = new QuantityLength(1.0, LengthUnit.YARD);
-		QuantityLength q4 = new QuantityLength(3.0, LengthUnit.FEET);
-
-		QuantityLength q5 = new QuantityLength(1.0, LengthUnit.CM);
-		QuantityLength q6 = new QuantityLength(0.393701, LengthUnit.INCH);
-
-		System.out.println(q1.equals(q2) ? "Equal (true)" : "Not Equal (false)");
-		System.out.println(q3.equals(q4) ? "Equal (true)" : "Not Equal (false)");
-		System.out.println(q5.equals(q6) ? "Equal (true)" : "Not Equal (false)");
+		System.out.println(q1.equals(q2));
 	}
 }
